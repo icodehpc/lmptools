0fee6118337d647b59b0476df7f5e1a96adfbb6c
Venkat Bala <balavk89@gmail.com>
2021-08-24 21:42:37 -0400

    create custom exception module with exception SkipSnapshot

---
+++ lmptools/exceptions.py
@@ -0,0 +1,13 @@
+class SkipSnapshot(Exception):
+    """
+    Exception to be raised when a snapshot is skipped while parsing a dump file
+    """
+    def __init__(self, msg: str):
+        super(SkipSnapshot, self).__init__()
+        self.msg = msg
+
+    def __str__(self):
+        if self.msg:
+            return f"SkipSnapshot, {self.msg}"
+        else:
+            return f"Exception SkipSnapshot occurred"
