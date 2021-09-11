3e27d261d95d84c743cbf29dff2a77f30e2b64c6
Venkat Bala <balv@bank-banque-canada.ca>
2021-09-10 22:06:00 -0400

    add unit tests to test each individual callback method

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -2,23 +2,69 @@ from lmptools.exceptions import SkipSnapshot
 import pytest
 import os
 import random
-import pandas as pd
 from typing import List
 from lmptools.atom import Atom
 from pydantic.tools import parse_obj_as
 from lmptools import Dump, DumpSnapshot, SimulationBox, DumpCallback
 
-class TestCallback(DumpCallback):
-    def on_snapshot_parse_time(self, timestamp: int, *args, **kwargs):
-        print(f"timestamp: {timestamp} parsed")
-
 class SkipSnapshotCallback(DumpCallback):
     """
-    This sample  callback will skip snapshot with timestamp less than 5000
+    This sample callback will skip all snapshots in a dump file
     """
     def on_snapshot_parse_timestamp(self, timestamp: int):
-        if timestamp < 5000:
+        if timestamp:
             raise SkipSnapshot("skipping snapshot")
+
+class OnSnapshotParseBegin(DumpCallback):
+    """
+    Callback to use to count the number of snapshots in a dumpfile
+
+    Use the on_snapshot_parse_begin callback to increment the `num_snapshots` attribute by 1
+    each time to count the snapshots
+    """
+    def __init__(self):
+        self.num_snapshots: int = 0
+
+    def on_snapshot_parse_begin(self, *args, **kwargs):
+        self.num_snapshots = self.num_snapshots + 1
+
+class OnSnapshotParseNatoms(DumpCallback):
+    def __init__(self):
+        self.natoms: List[int] = []
+
+    def on_snapshot_parse_natoms(self, natoms: int, *args, **kwargs):
+        self.natoms.append(natoms)
+
+class OnSnapshotParseTimestamp(DumpCallback):
+    def __init__(self):
+        self.timestamps: List[int] = []
+
+    def on_snapshot_parse_timestamp(self, timestamp: int, *args, **kwargs):
+        self.timestamps.append(timestamp)
+
+class OnSnapshotParseBox(DumpCallback):
+    """
+    Use this callback to cache the simulation box dimensions from each snapshot
+    """
+    def __init__(self):
+        self.simulation_box: List[SimulationBox] = []
+
+    def on_snapshot_parse_box(self, box: SimulationBox, *args, **kwargs):
+        self.simulation_box.append(box)
+
+class OnSnapshotParseAtoms(DumpCallback):
+    def __init__(self):
+        self.atoms: List[List[Atom]] = []
+
+    def on_snapshot_parse_atoms(self, atoms: List[Atom], *args, **kwargs):
+        self.atoms.append(atoms)
+
+class OnSnapshotParseEnd(DumpCallback):
+    def __init__(self):
+        self.snapshots: List[DumpSnapshot] = []
+
+    def on_snapshot_parse_end(self, snapshot: DumpSnapshot, *args, **kwargs):
+        self.snapshots.append(snapshot)
 
 @pytest.fixture(scope='session')
 def dump_file():
@@ -75,8 +121,78 @@ def dump_file():
     yield {'filename': filename, 'snapshots': snapshots}
     os.remove("dump.test.lammpstrj")
 
-
 def test_dump_snapshot_parse_iteration(dump_file):
     d = Dump(dump_file['filename'], unwrap=True)
     for index, snapshot in enumerate(d):
         assert snapshot == dump_file['snapshots'][index]
+
+def test_dump_callback_on_snapshot_begin_parse(dump_file):
+    """
+    Test whether the on snapshot parse begin callback has been invoked
+    """
+    cb = OnSnapshotParseBegin()
+    d = Dump(dump_file['filename'], callback=cb)
+    d.parse()
+    assert cb.num_snapshots == len(dump_file['snapshots'])
+
+def test_dump_callback_on_snapshot_parse_timestamp(dump_file):
+    """
+    Test whether on_snapshot_parse_atoms callback has been invoked
+    """
+    cb = OnSnapshotParseTimestamp()
+    d = Dump(dump_file['filename'], callback=cb)
+    d.parse()
+
+    for index, snapshot in enumerate(dump_file['snapshots']):
+        assert cb.timestamps[index] == snapshot.timestamp
+
+def test_dump_callback_on_snapshot_parse_natoms(dump_file):
+    """
+    Test whether on_snapshot_parse_atoms callback has been invoked
+    """
+    cb = OnSnapshotParseNatoms()
+    d = Dump(dump_file['filename'], callback=cb)
+    d.parse()
+
+    for index, snapshot in enumerate(dump_file['snapshots']):
+        assert cb.natoms[index] == snapshot.natoms
+
+def test_dump_callback_on_snapshot_parse_box(dump_file):
+    """
+    Assert that on_snapshot_parse_box callback is called
+    """
+    cb = OnSnapshotParseBox()
+    d = Dump(dump_file['filename'], callback=cb)
+    d.parse()
+
+    for index, snapshot in enumerate(dump_file['snapshots']):
+        assert cb.simulation_box[index] == snapshot.box
+
+def test_dump_callback_on_snapshot_parse_atoms(dump_file):
+    """
+    assert on_snapshot_parse_atoms is called
+    """
+    cb = OnSnapshotParseAtoms()
+    d = Dump(dump_file['filename'], callback=cb)
+    d.parse()
+
+    for index, snapshot in enumerate(dump_file['snapshots']):
+        assert cb.atoms[index] == snapshot.atoms
+
+
+def test_dump_callback_on_snapshot_parse_end(dump_file):
+    """
+    Assert on_snapshot_parse_end is called
+    """
+    cb = OnSnapshotParseEnd()
+    d = Dump(dump_file['filename'], callback=cb)
+    d.parse()
+
+    for index, snapshot in enumerate(dump_file['snapshots']):
+        assert cb.snapshots[index] == snapshot
+    
+def test_dump_skip_snapshot(dump_file):
+    cb = SkipSnapshotCallback()
+    d = Dump(dump_file['filename'], callback=cb, persist=True)
+    snapshots = d.parse()
+    assert snapshots == []
