0e40216a5febd8916b925dc018de30aeab7ebc4a
Venkat Bala <balavk89@gmail.com>
2022-08-22 01:03:48 +0000

    refactor source

--- lmptools/writers/sql/__init__.py
+++ lmptools/writers/sql/__init__.py
@@ -1,4 +1,11 @@
 from .models import AtomModel, Base, SimulationBoxModel, SimulationModel, TimestepModel
 from .sqlwriter import SQLWriter
 
-__all__ = ['SQLWriter', 'AtomModel', 'SimulationModel', 'SimulationBoxModel', 'TimestepModel', 'Base', ]
+__all__ = [
+    "SQLWriter",
+    "AtomModel",
+    "SimulationModel",
+    "SimulationBoxModel",
+    "TimestepModel",
+    "Base",
+]
