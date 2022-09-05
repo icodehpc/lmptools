1a3102c543bc559992e06ee8fc8c085149c70d92
Venkat Bala <balavk89@gmail.com>
2022-09-05 02:56:30 +0000

    add abstract base class, cleanup base dump parser

--- lmptools/dump/base.py
+++ lmptools/dump/base.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 from typing import List, Optional
-
+import os
 from loguru import logger
 from pydantic import parse_obj_as
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from ..core.atom import Atom
 from ..core.exceptions import SkipSnapshot
@@ -14,12 +14,26 @@ from ..core.simulation import DumpSnapshot, SimulationBox
 class DumpFileParser(ABC):
     """
     Base class defining the template for a LAMMPS dump file parser
+
+    Arg(s)
+
+    :param filename: Path to the dump file to be parsed
+    :param callback: [Optional] Callback to be used during parsing
     """
-    def __init__(self, filename: str):
-        self._filename = filename
+    def __init__(self, filename: str, callback: DumpCallback = None, unwrap: bool = False, verbose: bool = False):
+        self.filename = filename
+        if not os.path.exists(filename):
+            raise FileNotFoundError(f"Dump file {filename} not found")
+
+        self.callback = callback
+        self.unwrap = unwrap
+        self.verbose = verbose
+
+        self.file = open(self.filename)
 
     @abstractmethod
-    def parse(self)
+    def parse(self) -> Optional[DumpSnapshot]:
+        raise NotImplementedError
 
     @abstractmethod
     def __iter__(self):
@@ -33,7 +47,7 @@ class DumpFileParser(ABC):
         raise NotImplementedError
 
 
-class DumpCallback(object):
+class DumpCallback(ABC):
     """
     Base class used to build new callbacks that will be called as the dump file is parsed
 
@@ -53,127 +67,107 @@ class DumpCallback(object):
     def on_snapshot_parse_timestamp(self, timestamp: int, *args, **kwargs):
         """
         method to be called right after the snapshot timestamp is parsed
+
+        :param timestamp: Snapshot timestamp
         """
         pass
 
     def on_snapshot_parse_natoms(self, natoms: int, *args, **kwargs):
         """
         Method to be called right after number of atoms in the snapshot is parsed
+
+        :param natoms: Number of atoms in the snapshot
         """
         pass
 
     def on_snapshot_parse_box(self, box: SimulationBox, *args, **kwargs):
         """
         Method called when the simulation box info is parsed
+
+        :param box: Parsed simulation box for the snapshot
         """
         pass
 
     def on_snapshot_parse_atoms(self, atoms: List[Atom], *args, **kwargs):
         """
         Method called when atoms coordinates are parsed from file
+
+        :param atoms: List of all atoms parsed from the snapshot
         """
         pass
 
     def on_snapshot_parse_end(self, snapshot: DumpSnapshot, *args, **kwargs):
         """
         Method called when a snapshot has been completely parsed
+
+        :param snapshot: Dump snapshot just parsed
         """
         pass
 
 
-class CallbackList(DumpCallback):
+class Dump(DumpFileParser):
     """
-    Container to wrap multiple callbacks. Provides a cleaner interface to invoke the callback hooks
-    by invoking the function just once
-    """
-
-    def __init__(self, callbacks=None):
-        self.callbacks = callbacks
-        # If only a single instance is provided, wrap it as a list
-        if isinstance(callbacks, DumpCallback):
-            self.callbacks = [callbacks]
-
-    def on_snapshot_parse_begin(self, *args, **kwargs):
-        if self.callbacks:
-            for callback in self.callbacks:
-                callback.on_snapshot_parse_begin(*args, **kwargs)
-
-    def on_snapshot_parse_timestamp(self, timestamp: int, *args, **kwargs):
-        if self.callbacks:
-            for callback in self.callbacks:
-                callback.on_snapshot_parse_timestamp(timestamp, *args, **kwargs)
-
-    def on_snapshot_parse_natoms(self, natoms: int, *args, **kwargs):
-        if self.callbacks:
-            for callback in self.callbacks:
-                callback.on_snapshot_parse_natoms(natoms, *args, **kwargs)
-
-    def on_snapshot_parse_box(self, box: SimulationBox, *args, **kwargs):
-        if self.callbacks:
-            for callback in self.callbacks:
-                callback.on_snapshot_parse_box(box, *args, **kwargs)
-
-    def on_snapshot_parse_atoms(self, atoms: List[Atom], *args, **kwargs):
-        if self.callbacks:
-            for callback in self.callbacks:
-                callback.on_snapshot_parse_atoms(atoms, *args, **kwargs)
-
-    def on_snapshot_parse_end(self, snapshot: DumpSnapshot, *args, **kwargs):
-        if self.callbacks:
-            for callback in self.callbacks:
-                callback.on_snapshot_parse_end(snapshot, *args, **kwargs)
-
-
-class Dump(object):
-    """
-    Base Dump class to parse LAMMPS dump files
+    Dump class to parse LAMMPS dump files
     """
 
     def __init__(
         self,
         dump_file_name: str,
+        callback: DumpCallback,
         unwrap: bool = False,
-        callbacks: Optional[List[DumpCallback]] = None,
         verbose: bool = False,
     ):
-        self.snapshot: Optional[DumpSnapshot] = None
-        self.dump_file_name = dump_file_name
-        self.unwrap = unwrap
-        self.callbacks = CallbackList(callbacks=callbacks)
-        self.verbose = verbose
+        super().__init__(dump_file_name, callback, unwrap, verbose)
 
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> Optional[DumpSnapshot]:
         try:
-            self.file = open(dump_file_name)
-        except FileNotFoundError:
-            logger.error(f"{dump_file_name} not found")
+            snapshot = self.parse_snapshot()
+            if snapshot:
+                return snapshot
+            else:
+                raise StopIteration
+        except SkipSnapshot as e:
+            if self.verbose:
+                logger.info(f"{e}")
+                # Skip the remaining lines until next line starting with ITEM: TIMESTEP\n is read
+                while True:
+                    line = self.file.readline()
+                    if line == "ITEM: TIMESTEP\n":
+                        cur_pos = self.file.tell()
+                        self.file.seek(cur_pos - len("ITEM: TIMESTEP\n"))
+                        break
+                    elif line == "":
+                        # EOF is reached
+                        break
 
-    def parse_snapshot(self):
+    def parse_snapshot(self) -> Optional[DumpSnapshot]:
         """
         Read the dump file and return a single snapshot
         """
         snap: dict = {}
-        item = self.file.readline()  # +1
+        item = self.file.readline() # +1
 
-        # Readline with return an empty string if end of file is reached
-        if len(item) == 0:
-            self.snapshot = None
-            return
+        if item is None:
+            return None
 
         # Invoke on_snapshot_parse_begin callback
-        self.callbacks.on_snapshot_parse_begin()
+        self.callback.on_snapshot_parse_begin()
 
         timestamp = int(self.file.readline().split()[0])  # +1
         snap["timestamp"] = timestamp
 
         # Invoke on_snapshot_parse_timestamp callback
-        self.callbacks.on_snapshot_parse_timestamp(timestamp=timestamp)
+        self.callback.on_snapshot_parse_timestamp(timestamp=timestamp)
 
         item = self.file.readline()
         natoms = int(self.file.readline())  # +1
         snap["natoms"] = natoms
 
         # Invoke on_snapshot_parse_natoms callback
-        self.callbacks.on_snapshot_parse_natoms(natoms=natoms)
+        self.callback.on_snapshot_parse_natoms(natoms=natoms)
 
         item = self.file.readline()  # +1
         words = item.split("BOUNDS ")
@@ -229,7 +223,7 @@ class Dump(object):
         snap["box"] = SimulationBox(**box_dimensions)
 
         # Invoke on_snapshot_parse_box callback
-        self.callbacks.on_snapshot_parse_box(box=snap["box"])
+        self.callback.on_snapshot_parse_box(box=snap["box"])
 
         atoms: List[Atom] = []
         if natoms:
@@ -246,37 +240,15 @@ class Dump(object):
 
             snap["atoms"] = atoms
             # Invoke on_snapshot_parse_atoms callback
-            self.callbacks.on_snapshot_parse_atoms(atoms)
+            self.callback.on_snapshot_parse_atoms(atoms)
 
         # Create the snapshot
-        self.snapshot = parse_obj_as(DumpSnapshot, snap)
+        snapshot = parse_obj_as(DumpSnapshot, snap)
 
         # Invoke on_snapshot_parse_end callback
-        self.callbacks.on_snapshot_parse_end(snapshot=self.snapshot)
+        self.callback.on_snapshot_parse_end(snapshot=snapshot)
 
-    def __iter__(self):
-        return self
-
-    def __next__(self) -> Optional[DumpSnapshot]:
-        try:
-            self.parse_snapshot()
-            if self.snapshot:
-                return self.snapshot
-            else:
-                raise StopIteration
-        except SkipSnapshot as e:
-            if self.verbose:
-                logger.info(f"{e}")
-                # Skip the remaining lines until next line starting with ITEM: TIMESTEP\n is read
-                while True:
-                    line = self.file.readline()
-                    if line == "ITEM: TIMESTEP\n":
-                        cur_pos = self.file.tell()
-                        self.file.seek(cur_pos - len("ITEM: TIMESTEP\n"))
-                        break
-                    elif line == "":
-                        # EOF is reached
-                        break
+        return snapshot
 
     def parse(self) -> None:
         """
