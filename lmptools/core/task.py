80c013870523f6b9f92e85ff774402252978e529
Venkat Bala <balavk89@gmail.com>
2022-10-26 04:06:30 +0000

    add base task and pipline types

---
+++ lmptools/core/task.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from ..dump import DumpSnapshot
+from typing import Optional, List
+
+
+class Task(ABC):
+    """ Base Task class """
+
+    @abstractmethod
+    def run(self, snapshot: DumpSnapshot, *args, **kwargs) -> Optional[DumpSnapshot]:
+        """ Run method to implement the task """
+        raise NotImplementedError
+
+
+class Pipeline:
+    def __init__(self, tasks: List[Task]):
+        self._tasks = tasks
+
+    def run(self, snapshot: DumpSnapshot, *args, **kwargs):
+        for idx, task in enumerate(self.tasks):
+            snapshot = task.run(snapshot)
+    
+
+    
