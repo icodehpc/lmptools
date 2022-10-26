c9883103c4eef3c2f2b3ec1d7075e7a96bca04b8
Venkat Bala <balavk89@gmail.com>
2022-10-26 04:07:05 +0000

    add base task and pipline types

--- lmptools/core/task.py
+++ lmptools/core/task.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+
 from abc import ABC, abstractmethod
+from typing import List, Optional
+
 from ..dump import DumpSnapshot
-from typing import Optional, List
 
 
 class Task(ABC):
