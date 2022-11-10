2d2045ec66dd3cf57ef8e1da380e09967274f148
Venkat Bala <balavk89@gmail.com>
2022-11-10 03:35:06 +0000

    pre-commit ci fixes

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import os
-from setuptools import find_packages, setup, Extension
-from setuptools.command.build_ext import build_ext
 import subprocess
 import sys
+
+from setuptools import Extension, find_packages, setup
+from setuptools.command.build_ext import build_ext
 
 
 class CMakeExtension(Extension):
